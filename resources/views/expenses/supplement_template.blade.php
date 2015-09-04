e4339d95139d6cfcbca547630422b75ea695ff37
Joseph Richardson <EHLOVader@users.noreply.github.com>
2015-09-03 22:50:58 -0400

    Fix duplicate IDs

--- resources/views/expenses/supplement_template.blade.php
+++ resources/views/expenses/supplement_template.blade.php
@@ -2,14 +2,14 @@
     <div class="form-group js-new-supplement">
         {!! Form::label('name', 'Supplement name', ['class' => 'col-sm-2 control-label']) !!}
         <div class="col-sm-10">
-            {!! Form::text('name', null, array('class' => 'form-control', 'name' => 'supplement[new_{index}][name]' )) !!}
+            {!! Form::text('name{index}', null, array('class' => 'form-control', 'name' => 'supplement[new_{index}][name]' )) !!}
         </div>
     </div>
 
     <div class="form-group">
         {!! Form::label('amount', 'Amount', ['class' => 'col-sm-2 control-label']) !!}
          <div class="col-sm-10">
-             {!! Form::text('amount', null, array('class' => 'form-control', 'name' => 'supplement[new_{index}][amount]')) !!}
+             {!! Form::text('amount{index}', null, array('class' => 'form-control', 'name' => 'supplement[new_{index}][amount]')) !!}
          </div>
     </div>
 
